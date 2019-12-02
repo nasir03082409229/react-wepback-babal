d29a9208305528af5c65a9e518757f941883749b
nasir03082409229 <nasir03082409229@gmail.com>
2019-12-02 08:16:40 +0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+	context: __dirname,
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'main.js',
+		publicPath: '/',
+	},
+	devServer: {
+		historyApiFallback: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				exclude: /node_module/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						// presets: [
+						// 	['env', { 'modules': false }],
+						// 	// 'react' 
+						// ]
+					}
+				}
+			},
+			{
+				test: /\.css?$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(png|j?g|svg|gif)?$/,
+				use: 'file-loader'
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: path.resolve(__dirname, 'public/index.html'),
+			filename: 'index.html'
+		}),
+		new CleanWebpackPlugin(),
+	],
+	optimization: {
+		minimizer: [new UglifyJsPlugin()],
+	},
+};
